c34272d4e0688b74d04dd310969b80c131530a71
eyalraifler <eyalraifler3@gmail.com>
2026-01-29 18:46:42 +0200

    go over the data file and find all the occurences. right now everything seems t work well

--- scripts/find_sequences.py
+++ scripts/find_sequences.py
@@ -2,12 +2,12 @@ import re
 
 def find_dsal(seq):
     valid_seqs_found = re.findall(r'CCGCGG|CCGTGG|CCACGG|CCATGG', seq)
-    return len(valid_seqs_found)
+    return (valid_seqs_found, len(valid_seqs_found))
 
 def find_secl(seq):
     valid_seqs_found = re.findall(r'CC[ACGT][ACGT]GG', seq)
-    return len(valid_seqs_found)
+    return (valid_seqs_found, len(valid_seqs_found))
 
 def find_cjul(seq):
     valid_seqs_found = re.findall(r'CA[CT][ACGT]{5}[GA]TG', seq)
-    return len(valid_seqs_found)
+    return (valid_seqs_found, len(valid_seqs_found))
--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,5 @@
 import re
-from scripts.find_sequences import *
+from find_sequences import *
 
 
 file = open("data/orf_coding_all.fa.txt", "r")
@@ -21,6 +21,48 @@ if seq != "" and name != "": # to add the last gene in the file
     genes[name] = seq
 
 file.close()
+
+# Now we have a dictionary with all the genes and their seq. 
+# We can go over it and print all the genes that have at least one of the restriction sites.
+with open("results/restriction_sites_results.txt", "w") as results_file:
+    dsal_overall_count = 0
+    secl_overall_count = 0
+    cjul_overall_count = 0
+    genes_containing_sites = 0
+    
+    for gene_name in genes:
+        gene_seq = genes[gene_name]
+        dsal_count = find_dsal(gene_seq)[1]
+        dsal_overall_count += dsal_count
+        
+        secl_count = find_secl(gene_seq)[1]
+        secl_overall_count += secl_count
+        
+        cjul_count = find_cjul(gene_seq)[1]
+        cjul_overall_count += cjul_count
+        
+        secl_occurences = find_secl(gene_seq)[0]
+        cjul_occurences = find_cjul(gene_seq)[0]
+        dsal_occurences = find_dsal(gene_seq)[0]
+        
+        if dsal_count > 0 or secl_count > 0 or cjul_count > 0:
+            genes_containing_sites += 1
+            results_file.write(f"{gene_name}:\n")
+            results_file.write(f"There are {dsal_count} Dsal sites:\n")
+            for site in dsal_occurences:
+                results_file.write(f"Dsal site: {site}\n")
+                
+            results_file.write(f"There are {secl_occurences} Secl sites:\n")
+            for site in secl_occurences:
+                results_file.write(f"Secl site: {site}\n")
+            
+            results_file.write(f"There are {cjul_occurences} Cjul sites:\n")
+            for site in cjul_occurences:
+                results_file.write(f"Cjul site: {site}\n")
+
+    results_file.write(f"\nNumber of Protein with any kind of site: {genes_containing_sites}\n")
+        
+            
 
 
 
